801ac3d67fc4f236c863f290d8868774e18362cb
salmon <salmon@DESKTOP-EIBNCAA.thelocalgroup>
2022-06-25 19:34:25 -0400

    load plate file

--- main.groovy
+++ main.groovy
@@ -1,1 +1,3 @@
-//Your code here
+File plateUnitFile = ScriptingEngine.fileFromGit("https://github.com/NotOctogonapus/gridfinity_socket_holder.git", "isolated box plate v1.stl");
+CSG plateUnit  = Vitamins.get(plateUnitFile);
+return plateUnit
